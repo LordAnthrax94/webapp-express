bb05f805124f978c6e33338fd0552c159317b0e4
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-06 18:31:03 +0100

    Fix: inseriti nel controller ricerche dell'index e per lo show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,7 +2,7 @@ const connection = require("../data/db")
 
 const index = (req, res) =>{
  
-  const sql = 'SELECT * FROM movies'
+  const sql = 'SELECT * FROM movies';
 
   connection.query(sql, (err, results) =>{
     if(err) return res.status(500).json({error: 'Connessione al server fallita'})
@@ -12,7 +12,13 @@ const index = (req, res) =>{
 }
 
 const show = (req, res) => {
-  res.send('Dettaglio film')
+  const id = req.params.id
+  const sql = 'SELECT * FROM movies WHERE id = ?';
+  connection.query(sql, [id], (err, results) =>{
+    if(err) return res.status(500).json({error: 'Richiesta query non riuscita'});
+    if(results.length === 0) return res.status(404).json({error: 'Richiesta film non riuscita'})
+      res.json(results[0]);
+  })
 }
 
 module.exports ={
