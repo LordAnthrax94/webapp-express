7041989ddf12e19893c0e036aef77142c7d62f72
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-06 18:23:03 +0100

    fix: installato pacchetto dotenv e corretti nome per il db

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,2 +1,21 @@
+const connection = require("../data/db")
 
+const index = (req, res) =>{
+ 
+  const sql = 'SELECT * FROM movies'
 
+  connection.query(sql, (err, results) =>{
+    if(err) return res.status(500).json({error: 'Connessione al server fallita'})
+      res.json(results)
+  })
+
+}
+
+const show = (req, res) => {
+  res.send('Dettaglio film')
+}
+
+module.exports ={
+  index,
+  show
+}
--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get()
+const movieController = require("../controllers/moviesController")
+
+
+router.get('/', movieController.index)
+
+router.get('/:id', movieController.show)
+
+
+
+module.exports = router;
