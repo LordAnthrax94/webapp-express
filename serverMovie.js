d573c0b73bf7af15723e90b24d954cafe248b692
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-07 17:20:10 +0100

    fix: correzione della query show

--- serverMovie.js
+++ serverMovie.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const movieRouter = require('./routes/movieRouter');
 const errorHandlers = require('./middlewares/errorHand');
 const notFoundHandlers = require('./middlewares/notFound');
+const setImagePath = require('./middlewares/imgPath');
 
 app.use(cors({oring: 'http://localhost:5173/'}))
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.get('/', (req, res) =>{
   res.send('Lista Film')
 })
+app.use(setImagePath)
 
 app.use('/movies', movieRouter)
 
