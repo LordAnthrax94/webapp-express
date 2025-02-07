67a708e237171e460ee47aa2a4da65f90c21e52d
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-07 12:36:01 +0100

    fix: installato corse ma non messo in uso, aggiornato lo show per mostrare immagini

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,4 +1,5 @@
 const connection = require("../data/db")
+const setImagePath = require("../middlewares/imgPath")
 
 const index = (req, res) =>{
  
@@ -13,11 +14,17 @@ const index = (req, res) =>{
 
 const show = (req, res) => {
   const id = req.params.id
-  const sql = 'SELECT * FROM movies WHERE id = ?';
+  const sql = 'SELECT movies.*, reviews.vote, reviews.text FROM movies JOIN reviews ON reviews.movide_id = movies.id WHERE id = ?';
   connection.query(sql, [id], (err, results) =>{
     if(err) return res.status(500).json({error: 'Richiesta query non riuscita'});
     if(results.length === 0) return res.status(404).json({error: 'Richiesta film non riuscita'})
-      res.json(results[0]);
+      const movies = results.map(movie =>{
+        return{
+          ...movie,
+          image: `${req.imagePath}/img/movies/${movie.image}`
+        }
+    })
+      res.json(movies);
   })
 }
 
